b078f50dd833a3899636a24fce2cb03a9e6d0594
StutiRuparel <92007271+StutiRuparel@users.noreply.github.com>
2023-11-27 17:56:59 -0500

    Update FAQs.jsx

--- client/src/views/FAQs/FAQs.jsx
+++ client/src/views/FAQs/FAQs.jsx
@@ -1,14 +1,13 @@
-import React from "react"
-import NSF from "../../assets/nsf_logo.png"
-import TAMU from "../../assets/tamu_logo.png"
-import UF from "../../assets/uf_logo.png"
-import NavBar from "../../components/NavBar/NavBar"
-import "./FAQs.less"
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import { useSettings } from "../../settingsContext"; // Import the settings context
+import "../Help/Help.less";
 
 const faqs = [
   {
     question: "1. Can I access CASMM from my handheld device (mobile/tablet)?",
-    answer: "A- Yes, CASMM is accessible through different devices like tablets, 11.5-17 inch laptop screens.",
+    answer:
+      "A- Yes, CASMM is accessible through different devices like tablets, 11.5-17 inch laptop screens.",
   },
   {
     question: "2. Can I work with CASSM without making an account?",
@@ -20,9 +19,10 @@ const faqs = [
   },
   {
     question: "4. Does the site save cookies? If yes, how secure are the user accounts?",
-    answer: "A- Yes, cookies enable the user to resume their work from a previous session.\n" +
-    "The accounts are currently accessible using a secure student password.\n" +
-    "However, additional security measures are being implemented."
+    answer:
+      "A- Yes, cookies enable the user to resume their work from a previous session.\n" +
+      "The accounts are currently accessible using a secure student password.\n" +
+      "However, additional security measures are being implemented.",
   },
   {
     question: "5. Is CASMM screen-reader friendly?",
@@ -31,18 +31,44 @@ const faqs = [
 ];
 
 export default function FAQs() {
+  const { settings } = useSettings();
+
+  const answerStyle = {
+    fontFamily: settings.fontFamily,
+    fontSize:
+    settings.fontSize === "large" ||
+    settings.fontSize === "medium" ||
+    settings.fontSize === "small"
+      ? settings.fontSize
+      : '1.9em',
+    fontStyle: settings.fontStyle === 'italic' ? 'italic' : 'normal',
+    color: settings.fontColor, 
+    fontWeight: settings.fontStyle === 'bold' ? 'bold' : 'normal',
+  };
+  const titleStyle = {
+    fontFamily: settings.fontFamily,
+    fontSize: settings.fontSize,
+    fontStyle: settings.fontStyle === 'italic' ? 'italic' : 'normal',
+    color: settings.fontColor, 
+  };
+
+
   return (
     <div className="container nav-padding">
-      <NavBar />
+      
       <div className="faq-section">
-      <h1 id="title" class="white-text">FREQUENTLY ASKED QUESTIONS</h1>
+      
+        <h1 id="title" className={`white-text ${settings.fontStyle}`} style={titleStyle}>
+        <NavBar />
+          <h1 id="title" className={`white-text ${settings.fontStyle}`} style={titleStyle}>FREQUENTLY ASKED QUESTIONS</h1>
+        </h1>
 
         <div className="faq-box">
           <ul>
             {faqs.map((faq, index) => (
               <li key={index}>
-                <strong>{faq.question}</strong>
-                <p>{faq.answer}</p>
+                <strong style={answerStyle}>{faq.question}</strong>
+                <p style={answerStyle}>{faq.answer}</p>
               </li>
             ))}
           </ul>
